f8a5165dea3d0ad52e045aa7d45da48b9095f570
Alina Dubchak <alina.dubchak.ev@gmail.com>
2023-03-21 01:03:49 +0200

    added Circular LinkedList. Secont implemation

--- LinkedList.js
+++ LinkedList.js
@@ -1,79 +1,191 @@
-class LinkedList {
-  arr = [];
-  missEl = -1;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-  length = () => this.arr.length;
+class CircularLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+    this.misEl = -1;
+  }
+
+  length = () => {
+    return this.size;
+  };
 
   append = (element) => {
-    const isStr = typeof element;
-    if (element && isStr && element.length === 1) {
-      this.arr.push(element);
+    if (typeof element == "string" && element.length === 1) {
+      const node = new Node(element);
+      if (!this.head) this.head = node;
+      else this.tail.next = node;
+      node.next = this.head;
+      this.tail = node;
+      this.size++;
     } else throw new Error("Error. Wrong input data type, expected type char.");
   };
 
   insert = (element, index) => {
-    if (index < 0 || index > this.arr.length || this.arr[index] == undefined) {
-      throw new Error("Error. Wrong invalid index");
+    if (typeof element === "string" && element.length === 1)
+      if (index < 0 || index > this.size) {
+        throw new Error("Error. Wrong invalid index");
+      }
+    const node = new Node(element);
+    if (index === 0) {
+      node.next = this.head;
+      this.head = node;
+      this.tail = node;
+    } else if (index === this.size) {
+      this.tail.next = node;
+      this.tail = node;
+      node.next = this.head;
+    } else {
+      let current = this.head;
+      let i = 0;
+      while (i < index - 1) {
+        current = current.next;
+        i++;
+      }
+      node.next = current.next;
+      current.next = node;
     }
-    this.arr.splice(index, 0, element);
+    this.size++;
   };
 
   delete = (index) => {
-    if (index < 0 || index >= this.arr.length) {
+    if (index < 0 || index >= this.size) {
       throw new Error("Error. Wrong invalid index");
     }
-    return this.arr.splice(index, 1)[0];
+    let deletedItem = null;
+    if (this.size === 1) {
+      deletedItem = this.head.data;
+      this.head = null;
+      this.tail = null;
+    } else if (index === 0) {
+      deletedItem = this.head.data;
+      this.head = this.head.next;
+      this.tail.next = this.head;
+    } else {
+      let current = this.head;
+      let i = 0;
+      while (i < index - 1) {
+        current = current.next;
+        i++;
+      }
+      deletedItem = current.next.data;
+      current.next = current.next.next;
+      if (index === this.size - 1) this.tail = current;
+    }
+    this.size--;
+    return deletedItem;
   };
 
   deleteAll = (data) => {
-    this.arr = this.arr.filter((element) => element !== data);
+    let current = this.head;
+    let prev = this.tail;
+    let i = 0;
+    while (i < this.size) {
+      if (current.data === data) {
+        if (i === 0) {
+          this.head = this.head.next;
+          this.tail.next = this.head;
+          prev = this.tail;
+        } else {
+          prev.next = current.next;
+          if (i === this.size - 1) this.tail = prev;
+        }
+        this.size--;
+        i--;
+      } else {
+        prev = current;
+      }
+      current = current.next;
+      i++;
+    }
   };
 
   get = (index) => {
-    if (index < 0 || index >= this.arr.length) {
+    if (index < 0 || index >= this.size) {
       throw new Error("Error. Wrong invalid index");
     }
-    return this.arr[index];
+    let current = this.head;
+    let i = 0;
+    while (i < index) {
+      current = current.next;
+      i++;
+    }
+    return current.data;
   };
 
   clone = () => {
-    const newList = new LinkedList();
-    for (let i = 0; i < this.arr.length; i++) {
-      newList.append(this.arr[i]);
+    const newList = new CircularLinkedList();
+    let current = this.head;
+    let i = 0;
+    while (i < this.size) {
+      newList.append(current.data);
+      current = current.next;
+      i++;
     }
     return newList;
   };
 
   reverse = () => {
-    this.arr.reverse();
+    let current = this.head;
+    let prev = this.tail;
+    let i = 0;
+    while (i < this.size) {
+      const next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+      i++;
+    }
+    this.head = prev;
+    if (this.size > 0) this.tail = this.head.next;
   };
 
   findFirst = (element) => {
-    for (let i = 0; i < this.arr.length; i++) {
-      if (this.arr[i] === element) {
+    let current = this.head;
+    let i = 0;
+    while (i < this.size) {
+      if (current.data === element) {
         return i;
       }
+      current = current.next;
+      i++;
     }
-    return this.missEl;
+    return this.misEl;
   };
 
   findLast = (data) => {
-    for (let i = this.arr.length - 1; i >= 0; i--) {
-      if (this.arr[i] === data) {
-        return i;
-      }
+    let current = this.head;
+    let i = 0;
+    while (i < this.size) {
+      if (current.data === data) this.misEl = i;
+      current = current.next;
+      i++;
     }
-    return this.missEl;
+    return this.misEl;
   };
 
-  clear = () => (this.arr = []);
+  clear = () => {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  };
 
   extend(arr) {
-    for (let i = 0; i < arr.length(); i++) {
-      this.arr.push(arr.get(i));
+    let current = arr.head;
+    let i = 0;
+    while (i < arr.size) {
+      this.append(current.data);
+      current = current.next;
+      i++;
     }
-    return this.arr;
   }
 }
 
-module.exports = LinkedList;
+module.exports = CircularLinkedList;
